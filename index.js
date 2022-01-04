485b75ceef727ddd4524e8dd043b844d1a9888c9
Miriam Farkas <miriamjoshua00@gmail.com>
2022-01-04 21:27:10 +0200

    commit tree 299d976081722efcbf6879451b08202374cd4e93

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const headquarters = 42
+const distanceFromHqInBlocks = function(pickupLocation) {
+    if (pickupLocation>headquarters){
+        return pickupLocation - headquarters
+    }
+    else if (pickupLocation<headquarters) {
+        return headquarters - pickupLocation
+    }
+}
+
+const distanceFromHqInFeet = function(blocks) {
+   
+    return (distanceFromHqInBlocks(blocks) * 264)
+}
+
+const distanceTravelledInFeet = function(startingBlock, endingBlock) {
+    console.log("calculating distance in feet")
+    if (endingBlock>startingBlock) {
+        return (endingBlock - startingBlock)*264
+    } else if (endingBlock<startingBlock) {
+        return (startingBlock - endingBlock)*264
+    }   
+}
+
+distanceTravelledInFeet(13, 43)
+
+function calculatesFarePrice(starting, destination) {
+    const distance = distanceTravelledInFeet(starting, destination)
+    if (distance<= 400) {
+        return 0
+    } else if (distance>400 && distance<2000) {
+        return (distance - 400)*0.02
+    } else if (distance>2000 && distance<=2500) {
+        return 25
+    } else if (distance>2500) {
+        return "cannot travel that far"
+    }
+}
